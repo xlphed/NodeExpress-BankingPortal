ea267561fe3c195b663c1ccc47f915b548655d6a
Alfred Lee <wei.si.lee@accenture.com>
2023-01-21 03:21:22 +0800

    Completed module

--- src/app.js
+++ src/app.js
@@ -12,6 +12,12 @@ app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "/public")));
 app.use(express.urlencoded({ extended: true }));
 
+const accountRoutes = require("./routes/accounts.js");
+const servicesRoutes = require("./routes/services.js");
+
+app.use("/account", accountRoutes);
+app.use("/services", servicesRoutes);
+
 app.get("/", (req, res) => {
 	res.render("index", {
 		title: "Account Summary",
@@ -19,61 +25,9 @@ app.get("/", (req, res) => {
 	});
 });
 
-app.get("/savings", (req, res) => {
-	res.render("account", {
-		account: accounts.savings,
-	});
-});
-
-app.get("/checking", (req, res) => {
-	res.render("account", {
-		account: accounts.checking,
-	});
-});
-
-app.get("/credit", (req, res) => {
-	res.render("account", {
-		account: accounts.credit,
-	});
-});
-
 app.get("/profile", (req, res) => {
 	res.render("profile", {
 		user: users[0],
-	});
-});
-
-app.get("/transfer", (req, res) => {
-	res.render("transfer");
-});
-
-app.post("/transfer", (req, res) => {
-	accounts[req.body.from].balance =
-		accounts[req.body.from].balance - req.body.amount;
-	accounts[req.body.to].balance =
-		parseInt(accounts[req.body.to].balance) + parseInt(req.body.amount, 10);
-	const accountsJSON = JSON.stringify(accounts, null, 4);
-	writeJSON();
-	res.render("transfer", {
-		message: "Transfer Completed",
-	});
-});
-
-app.get("/payment", (req, res) => {
-	res.render("payment", {
-		account: accounts.credit,
-	});
-});
-
-app.post("/payment", (req, res) => {
-	accounts.credit.balance = accounts.credit.balance - req.body.amount;
-	accounts.credit.available =
-		parseInt(accounts.credit.available) + parseInt(req.body.amount);
-	const accountsJSON = JSON.stringify(accounts, null, 4);
-	writeJSON();
-	res.render("payment", {
-		message: "Payment Successful",
-		account: accounts.credit,
 	});
 });
 
