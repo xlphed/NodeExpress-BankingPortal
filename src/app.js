e9c4da221ae3097d8c456d850d04a21ddc2eba64
Alfred Lee <wei.si.lee@accenture.com>
2023-01-21 02:30:11 +0800

    module3

--- src/app.js
+++ src/app.js
@@ -8,16 +8,17 @@ app.set("views", path.join(__dirname, "/views"));
 app.set("view engine", "ejs");
 
 app.use(express.static(path.join(__dirname, "/public")));
+app.use(express.urlencoded({ extended: false }));
 
 const accountData = fs.readFileSync(
 	path.join(__dirname, "/json/accounts.json"),
-	"UTF8"
+	"utf8"
 );
 const accounts = JSON.parse(accountData);
 
 const userData = fs.readFileSync(
 	path.join(__dirname, "/json/users.json"),
-	"UTF8"
+	"utf8"
 );
 const users = JSON.parse(userData);
 
@@ -49,6 +50,48 @@ app.get("/credit", (req, res) => {
 app.get("/profile", (req, res) => {
 	res.render("profile", {
 		user: users[0],
+	});
+});
+
+app.get("/transfer", (req, res) => {
+	res.render("transfer");
+});
+
+app.post("/transfer", (req, res) => {
+	const { from, to, amount } = req.body;
+	accounts[from].balance = accounts[from].balance - amount;
+	accounts[to].balance = parseInt(accounts[to].balance) + parseInt(amount);
+	const accountsJSON = JSON.stringify(accounts);
+	console.log(accountsJSON);
+	fs.writeFileSync(
+		path.join(__dirname, "/json/accounts.json"),
+		accountsJSON,
+		"UTF8"
+	);
+	res.render("transfer", {
+		message: "Transfer Completed",
+	});
+});
+
+app.get("/payment", (req, res) => {
+	res.render("payment", {
+		account: accounts.credit,
+	});
+});
+
+app.post("/payment", (req, res) => {
+	accounts.credit.balance = accounts.credit.balance - req.body.amount;
+	accounts.credit.available =
+		parseInt(accounts.credit.available) + parseInt(req.body.amount);
+	const accountsJSON = JSON.stringify(accounts);
+	fs.writeFileSync(
+		path.join(__dirname, "/json/accounts.json"),
+		accountsJSON,
+		"UTF8"
+	);
+	res.render("payment", {
+		message: "Payment Successful",
+		account: accounts.credit,
 	});
 });
 
