99f3deac4859ca12050b2862c77c127c2c387748
Alfred Lee <wei.si.lee@accenture.com>
2023-01-21 01:53:52 +0800

    module2

--- src/app.js
+++ src/app.js
@@ -9,9 +9,46 @@ app.set("view engine", "ejs");
 
 app.use(express.static(path.join(__dirname, "/public")));
 
+const accountData = fs.readFileSync(
+	path.join(__dirname, "/json/accounts.json"),
+	"UTF8"
+);
+const accounts = JSON.parse(accountData);
+
+const userData = fs.readFileSync(
+	path.join(__dirname, "/json/users.json"),
+	"UTF8"
+);
+const users = JSON.parse(userData);
+
 app.get("/", (req, res) => {
 	res.render("index", {
-		title: "Index",
+		title: "Account Summary",
+		accounts: accounts,
+	});
+});
+
+app.get("/savings", (req, res) => {
+	res.render("account", {
+		account: accounts.savings,
+	});
+});
+
+app.get("/checking", (req, res) => {
+	res.render("account", {
+		account: accounts.checking,
+	});
+});
+
+app.get("/credit", (req, res) => {
+	res.render("account", {
+		account: accounts.credit,
+	});
+});
+
+app.get("/profile", (req, res) => {
+	res.render("profile", {
+		user: users[0],
 	});
 });
 
