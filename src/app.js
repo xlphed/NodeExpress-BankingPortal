5f0614089bdffdd5ec3a952dd85a000961404267
Alfred Lee <wei.si.lee@accenture.com>
2023-01-21 01:32:29 +0800

    module1

--- src/app.js
+++ src/app.js
@@ -4,10 +4,10 @@ const express = require("express");
 
 const app = express();
 
-app.set("views", path.join(__dirname, "/views/"));
+app.set("views", path.join(__dirname, "/views"));
 app.set("view engine", "ejs");
 
-app.use(express.static(path.join(__dirname, "/public/")));
+app.use(express.static(path.join(__dirname, "/public")));
 
 app.get("/", (req, res) => {
 	res.render("index", {
